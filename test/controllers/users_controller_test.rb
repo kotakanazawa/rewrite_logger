8779a62bcb067ff02bad72bf4df9f8e9e42fd1b9
Kota Kanazawa <kota0818@gmail.com>
2020-08-18 17:14:57 +0900

    bin/lint

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,10 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get users_index_url
     assert_response :success
   end
-
 end
