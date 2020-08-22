0a4b0a226498ba781322ecbcc97484b4359c4d5a
Kota Kanazawa <kota0818@gmail.com>
2020-08-22 11:54:37 +0900

    カラム名変更: username → name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_021923) do
+ActiveRecord::Schema.define(version: 2020_08_22_025246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_08_22_021923) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
--- spec/factories/rankings.rb
+++ spec/factories/rankings.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :ranking do
     ranking { 1 }
-    article { nil }
+    article
   end
 end
