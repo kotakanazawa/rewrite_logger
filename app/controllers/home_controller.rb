a0450cdbc938ff0e4acc20eaeeef072f9193e94e
Kota Kanazawa <kota0818@gmail.com>
2020-09-13 16:09:14 +0900

    ログインしてたら記事一覧、してなければランディングページを表示

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class HomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    if user_signed_in?
+      @articles = current_user.articles
+      @today = Date.current
+      @start_date = Date.current.ago(14.days).to_date
+    else
+      render "welcome/index"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "articles#index"
+  root "home#index"
 
   devise_for :users
   resources :articles do
     resources :logs, only: %i(new create update)
   end
   resources :logs, only: %i(show destroy edit)
+
+  get "/tos", to: "welcome#tos"
+  get "/policy", to: "welcome#policy"
 end
