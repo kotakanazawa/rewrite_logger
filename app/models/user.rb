ad6bae609556c4cfa866a6a8641fd03b5f0bda7d
Kota Kanazawa <kota0818@gmail.com>
2020-09-14 09:00:30 +0900

    emailに一意制約

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, presence: true, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
