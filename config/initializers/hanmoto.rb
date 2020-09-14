ab17cd61f8b622c6d9f172890949f61c726a0e01
Kota Kanazawa <kota0818@gmail.com>
2020-09-14 22:13:27 +0900

    bin/lint

--- config/initializers/hanmoto.rb
+++ config/initializers/hanmoto.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Hanmoto.configure do |config|
-  config.view_dir = 'public_pages'
+  config.view_dir = "public_pages"
   config.layouts = {
-    html: 'public'
+    html: "public"
   }
 end
