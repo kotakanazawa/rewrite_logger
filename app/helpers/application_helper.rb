8207a4c858f2a9f8786df7b609aee94998c76050
Kota Kanazawa <kota0818@gmail.com>
2020-09-16 21:08:21 +0900

    記事詳細のURLをハイパーリンクにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def url_to_link(text)
-    URI.extract(text, ["http", "https"]).uniq.each do |url|
-      text.gsub!(url, "#{url}")
-    end
-    text
-  end
-
   def ranking_chart(rankings)
     line_chart rankings,
       min: 1,
